395e241674712de2f43a4d6bf04fd9337e628504
Vicko <victorgsosa@gmail.com>
2021-07-14 09:10:36 -0500

    added integration packs

--- env/chef/roles/cxcomm.rb
+++ env/chef/roles/cxcomm.rb
@@ -8,11 +8,11 @@ override_attributes 'cxcomm' => {
 	'install' => {
 		'version' => '2005',
 		'download_url' => 'file:///tmp/cx/CXCOM200500P_14-70004955.zip',
-		'download_checksum' => 'ea93ca6cc7503dcd1e82c2f9aeb5f4733d92611dfa3d9ca42bc116151676ccd4',
+		'download_checksum' => '2738afa2e40e6a3b2a01c6ba805c3a3938c8bbe13e99a1acb01b5520710fee35',
 		'install_spartacus' => true,
 		'install_ip' => true,
 		'ip_download_url' => 'file:///tmp/cx/CXCOMINTPK200500P_0-80005540.zip',
-		'ip_download_checksum' => '7bf5d9326c38bcb759288422c56577f1da1e5931b4e36cc1c7f04aa7fa76eb04'
+		'ip_download_checksum' => 'afe8b7be15eb3d96e44f70e802a22aa34e979c78ab09a38121592ef9144ac079'
 	}
 }
 
