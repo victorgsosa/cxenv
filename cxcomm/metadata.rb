de0c3d702d02ad9541d48bf1f62c49acde6f6ed1
Vicko <victorgsosa@gmail.com>
2021-07-23 15:01:34 -0500

    change chef version

--- cxcomm/metadata.rb
+++ cxcomm/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'you@example.com'
 license 'All Rights Reserved'
 description 'Installs/Configures cxcomm'
 version '0.1.0'
-chef_version '>= 12.0'
+chef_version '>= 14.0'
 
 # The `issues_url` points to the location where issues for this cookbook are
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
